813f10bde2a32ac718ba4515bf03d4aeb0989d5b
diogofonte <diogo.fonte2000@gmail.com>
2023-03-15 15:33:10 +0000

    init tp5

---
+++ tp5/sum_numbers/sum2.php
@@ -0,0 +1,9 @@
+<?php
+
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$sum = $num1 + $num2;
+
+echo $num1 . " + " . $num2 . " = " . $sum;
+
+echo "<div><br><a href=\"form2.html\">Do another sum</a></div>";
